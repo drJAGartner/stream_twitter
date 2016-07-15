2aec86912ce90b65a69b6233d8f7807d3c506583
Luke Wendling <luke@lukewendling.com>
2016-07-15 14:00:04 -0500

    set mkdirs mode

--- util/dirtools.py
+++ util/dirtools.py
@@ -4,7 +4,7 @@ import os, errno
 def mkdir_p(path):
     """ 'mkdir -p' in Python > 2.5"""
     try:
-        os.makedirs(path)
+        os.makedirs(path, 0777)
     except OSError as exc:
         # pass if already exists
         if exc.errno == errno.EEXIST and os.path.isdir(path):
